5f806b7be1314419b48369a0770449c5c753ec6b
Hossam <hossamsoliuman@gmail.com>
2024-06-30 17:31:19 +0300

    update app name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ACMS</title>
+    <title>Binghubs</title>
     <link rel="icon" href="logo-white-trans.png">
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="logo-white.png">
 
-    <title>ACMS</title>
+    <title>Binghubs</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css">
